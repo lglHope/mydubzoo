577ef60beb1412514a95abd79dd57a6884c360e4
lglHope <l1911739019@qq.com>
2018-12-01 15:13:04 +0800

    IDEA整合Git,CityController所以RESTFUL风格获取参数的两种方式

--- src/main/java/liu/hope/mydubzoo/service/CityService.java
+++ src/main/java/liu/hope/mydubzoo/service/CityService.java
@@ -2,6 +2,7 @@ package liu.hope.mydubzoo.service;
 
 import liu.hope.mydubzoo.mapper.CityMapper;
 import liu.hope.mydubzoo.pojo.City;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -16,8 +17,11 @@ public class CityService {
     @Resource
     private CityMapper cityMapper;
 
+    private RedisTemplate<String,City> redisTemplate;
+
     public City getCityById(int id){
         City city = cityMapper.getCityById(id);
+        redisTemplate.opsForValue().set(id+"",city);
         return city;
     }
 
